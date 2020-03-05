b1b36cd01cca1d99b648a7ef3c4c211d973f6291
szymon242820 <58854456+szymon242820@users.noreply.github.com>
2020-03-05 12:38:42 +0100

    Add files via upload

---
+++ lekcja1/zad6.py
@@ -0,0 +1,2 @@
+tekst = "Minimini życie nie Minimini, nie Minimini, niemili tu nie lubili mnie Minimini zło, nie Minimini, nie Minimini, na miny tu chcieli w**bać mnie"
+print(tekst.count("Minimini"))
